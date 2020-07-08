3fea49e5544a56295276f2e0d065153e5f897cc7
“First <Kerger06@aol.com>
2020-07-08 13:51:24 -0500

    another merge

--- src/components/CategoriesList/index.js
+++ src/components/CategoriesList/index.js
@@ -3,14 +3,13 @@ import "./categoriesList.css";
 
 function CategoriesList() {
   
-  //const URL = 'https://api.spotify.com/v1/me/#access_token=BQB7S01M2xAW5jNXKoBeEiNNtKABTPB7Y9SnxTEz-gg3qReUj9G-EKEcl2rovtVJo5_SxADXc0H8ez17WWF8b6sLt1Z3B2bh06yWCy2MKARr3QUESz5V4lBRrSowRwOd-RiOvCyrCAzeyViz7AxZvgPxWkSXcys&token_type=Bearer&expires_in=3600&state=123';
+  
   //https://jsonplaceholder.typicode.com/users
 
   const [category, setCategory] = useState([]);
 
   useEffect(() => {
     let URL = window.location;
-    console.log(URL.hash.split('&')[0].split('=')[1]);
     let access_token = URL.hash.split('&')[0].split('=')[1];
     fetch('https://api.spotify.com/v1/browse/categories',
       {
@@ -21,12 +20,13 @@ function CategoriesList() {
       })
       .then((response) => response.json())
       .then((response) => setCategory(response.categories.items))
+      
 
 
   }, []);
 
   return (
-    <div className="App">
+    <div className="card-group">
       
 
       {

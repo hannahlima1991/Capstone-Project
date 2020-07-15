418f90e9b4bc56abde10b6b3309dc2af482ee81d
“First <Kerger06@aol.com>
2020-07-15 14:40:32 -0500

    styling for music player

--- src/components/MusicPlayer/index.js
+++ src/components/MusicPlayer/index.js
@@ -3,46 +3,42 @@ import "./musicPlayer.css";
 import "../ListOfSongs";
 import "../CategoriesList";
 import "../ListOfPlayLists";
+import MusicVideoIcon from '@material-ui/icons/MusicVideo';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
+import CodeIcon from '@material-ui/icons/Code';
 
 function MusicPlayer() {
   const [selectedSong, setSelectedSong] = useState("");
 
-  function handleClick(e) {
-    let name = e.target.name;
-    let value = e.target.value;
-    const selectedSong = `https://open.spotify.com/embed/${name}/${value}`;
-    console.log(selectedSong);
-    setSelectedSong(selectedSong);
-  }
+  // function handleClick(e) {
+  //   let name = "album"
+  //   let value = "46lFttIf5hnUZMGvjK0Wxo"
+  //   const selectedSong = `https://open.spotify.com/embed/${name}/${value}`;
+  //   console.log(selectedSong);
+  //   setSelectedSong(selectedSong);
+  // }
 
   return (
-    <div className="App">
-      <h1>Music Player</h1>
-      <button name="track" value="1bPeGzT9e95wpzs3ovNwFB" onClick={handleClick}>
-        No way out - Vicetone
-      </button>
-      <button name="track" value="46lFttIf5hnUZMGvjK0Wxo" onClick={handleClick}>
-        Runaway(U & I) - Galantis{" "}
-      </button>
-      <button
-        name="album"
-        value="1zJ1gKd6steCSa30EPO6M5?si=Wh4HIafTTq2f_hGAmw5N2Q"
-        onClick={handleClick}
-      >
-        album Hardwell United we are
-      </button>
-      {selectedSong !== [] ? (
-        <iframe
-          src={selectedSong}
-          width="300"
-          height="380"
-          frameborder="0"
-          allowtransparency="true"
-          allow="encrypted-media"
-        ></iframe>
-      ) : null}
-    </div>
-  );
-}
+      <div class="jumbotron">
+      <div className="header">
+      <h1 className = "title animate-reveal animate-first display-4">
+      <MusicVideoIcon style={{ fontSize: 80 }}/>
+          Spotibea Music Player   
+       <LoyaltyIcon style={{ fontSize: 80 }}/>
+      </h1>
+      </div>
+      <div className = "resp-container animate-reveal animate-second">
+      <iframe className = "resp-iframe "
+      src="https://open.spotify.com/embed/album/1zJ1gKd6steCSa30EPO6M5" 
+       
+      frameborder="20" 
+      allowtransparency="true" 
+      allow="encrypted-media">
+      </iframe>
+  </div>
+ </div>
+  
+  )}
+
 
 export default MusicPlayer;

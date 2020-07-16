4f11b8dc38f3769c7db842692e570050fea62584
Steve Medrano <60121429+darknight101-pixel@users.noreply.github.com>
2020-07-16 10:35:38 -0500

    displaying songs reading from list of playlist id

--- src/components/ListOfSongs/index.js
+++ src/components/ListOfSongs/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./listOfSongs.css";
-import "../ListOfPlayLists";
-import "../CategoriesList";
+import ListOfPlayLists from "../ListOfPlayLists";
+import CategoriesList from "../CategoriesList";
 
 let URL = window.location;
 let access_token = URL.hash.split("&")[0].split("=")[1];
@@ -15,7 +15,6 @@ function ListOfSongs(props) {
 
   const getSongLists = (id) => {
     let apiRequest = `https://api.spotify.com/v1/playlists/${id}/tracks`;
-
     fetch(apiRequest, {
       headers: {
         Authorization: "Bearer " + access_token,
@@ -23,8 +22,36 @@ function ListOfSongs(props) {
       method: "GET",
     })
       .then((response) => response.json())
-      .then((response) => console.log("A list of songs", response));
+      .then((response) => {
+        console.log("A list of songs", response);
+        setSongList(response.items);
+      });
   };
+  return (
+    <div className="songsList">
+      <h1>Hello</h1>
+      <div className="card">
+        {songList.map((song) => {
+          const { track } = song;
+          const { name = "" } = track;
+          const { duration_ms } = track;
+
+          return (
+            <div className="SongUI">
+              <div className="card">
+                <div className="card-body lg-12">
+                  {name}
+                  <button>push</button>
+                </div>
+              </div>
+
+              {/* <p>{duration_ms}</p>; */}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 export default ListOfSongs;
 
